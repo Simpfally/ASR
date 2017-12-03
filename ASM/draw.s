066359bd72787c63883619d8b0cd2408a78da562
Potatoe37 <34173967+Potatoe37@users.noreply.github.com>
2017-12-03 10:50:00 +0100

    Update draw.s

--- ASM/draw.s
+++ ASM/draw.s
@@ -1,1 +1,8 @@
+;;draw
+;; trace une ligne de couleur r0 entre les points de coordonnée (r1,r2) et (r3,r4)
+;; Comment faire pour tracer des lignes de bas en haut et de haut en bas??
+jump 16 main
 
+draw:
+;; r5 est l'erreur
+sub3 r5 r1 r3
