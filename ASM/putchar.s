5c25cb9d9d90c7e4d468a1120f4c4451877f6cd5
Potatoe37 <34173967+Potatoe37@users.noreply.github.com>
2017-12-03 21:57:18 +0100

    Update putchar.s

--- ASM/putchar.s
+++ ASM/putchar.s
@@ -1,9 +1,14 @@
 ;;putchar
-;;Affiche un caractère codé dans r3 de la couleur r0 aux coordonnées (r1,r2)
+;;Affiche un caractère codé dans r3+r4 de la couleur r0 aux coordonnées (r1,r2)
 
 jump 16 main
 
 putchar:
+push 32 r3
+push 32 r4
+
+
+
 
 return
 
