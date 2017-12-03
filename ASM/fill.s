1dd1900c1a2fa665f2cab2b03c8e6c64295cefbe
Simpfally <simpfally@gmail.com>
2017-12-03 16:40:01 +0100

    corrigé fill.s

--- ASM/fill.s
+++ ASM/fill.s
@@ -1,6 +1,9 @@
 ;; fill
 ;; remplit un rectangle (r1,r2),(r3,r4) de la couleur r0 avec la convention (r1>r3)&(r2>r4)
 
+
+;; Bug : ne fait que la première ligne pour le moment je crois
+
 jump 16 main
 
 fill:
@@ -20,23 +23,23 @@ add2i r5 0x10000
 
 ;; pour savoir de combien se décaler après une ligne
 leti r6 160
-sub r6 r3
-add r6 r1
+sub2 r6 r3
+add2 r6 r1
 shift 0 r6 4
 
 push 16 r1
 loop0:
-add r2 1
+add2i r2 1 ;;add2i?
 pop 16 r1
 push 16 r1
 setctr a0 r5
 loop1:
 write a0 16 r0
-addi r5 16
-addi r1 1
+add2i r5 16
+add2i r1 1
 cmp r3 r1
 jumpif 16 ge loop1
-addi r5 r6
+add2 r5 r6
 cmp r4 r2
 jumpif 16 ge loop0
 
