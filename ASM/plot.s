35443016b59a34d48ab47fd10eda98414466af7b
Simpfally <simpfally@gmail.com>
2017-12-03 16:47:12 +0100

    plot.s fonctionne maintenant

--- ASM/plot.s
+++ ASM/plot.s
@@ -1,5 +1,10 @@
 ;;plot
 ;;affiche un point de couleur r0 aux coordonnées (r1,r2)
+
+;; (0,0) -----> (159, 0)
+;;   |
+;;   V
+;; (0, 127)
 jump 16 main
 
 plot:
@@ -12,7 +17,7 @@ add2 r3 r2
 shift 0 r2 2
 add2 r3 r2
 shift 0 r3 4
-add2 r3 0x10000
+add2i r3 0x10000
 
 setctr a0 r3
 write a0 16 r0
@@ -23,7 +28,19 @@ return
 
 main:
 leti r0 0xF123
-leti r1 10
-leti r2 10
+leti r1 0
+leti r2 0
+call plot
+leti r1 1
+leti r2 0
+call plot
+leti r1 100
+leti r2 0
+call plot
+leti r1 159
+leti r2 0
+call plot
+leti r1 0
+leti r2 127
 call plot
 jump -13
