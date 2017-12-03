0ff4d91a3629432334efda1a6590623bea5d385b
Potatoe37 <34173967+Potatoe37@users.noreply.github.com>
2017-12-03 13:03:57 +0100

    In process...

--- ASM/draw.s
+++ ASM/draw.s
@@ -11,14 +11,12 @@ push 16 r7
 sub3 r6 r4 r2 ;;dy ← y2 - y1
 sub3 r5 r1 r3 ;;dx ← x2 - x1 et e ← -dx (valeur d’erreur initiale)
 ;; y ← y1: on utilisera directement r2 (rangée initiale)
-;; pour ces deux la il faut voir comment gerer la pile et r7
-;; sachant que r7 sera écrasé à l'execution de call plot 
-;;donc il faudra l'empiler juste avant et le dépiler juste après (c'est pas un probleme mais c'est important^^)
 shift 0 r6 1 ;;e(1,0) ← dy × 2 
-let r7 r6
+let r7 r5
 shift r7 1 ;;e(0,1) ← -dx × 2  (si le shift marche pour les négatifs)
 
 loop:
+;;la valeur de plot est écrasée à l'appel de call donc il faut l'empiler juste avant et le dépiler juste après
 push 16 r7
 call plot
 pop 16 r7
@@ -26,7 +24,7 @@ add2i r1 1
 
 add2 r5 r6
 cmpi r5 0
-jumpif lt 16 endif ;;if (e ← e+e(1,0) >= 0) Ici je sais pas si la valeur de e doit etre modifiée mais je pense que oui 
+jumpif lt 16 endif ;;if (e ← e+e(1,0) >= 0) Ici je sais pas si la valeur de e doit etre modifiée mais je pense que oui (c'est fait) 
 add2i r2 1 ;;y 
 add2 r5 r7 ;;e ← e+e(0,1)
 endif:
