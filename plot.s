21c42ea4559ab53522c68a9e9dad31b14ef2ea0b
Potatoe37 <34173967+Potatoe37@users.noreply.github.com>
2017-12-03 09:09:05 +0100

    Update plot.s

--- plot.s
+++ plot.s
@@ -1,8 +1,28 @@
+jump 16 main
+
 plot:
 
 leti r3 0x10000
 setsctr a0 r3
 
-;;décalage.... Je vois pas commetn faire autrement
-loop:
-add2 r1 r1
+write a0 16 r0
+;;décalage.... Je vois pas comment faire autrement
+add2 r2 r2
+add2 r2 r2
+add2 r2 r2
+add2 r2 r2
+add2 r2 r2
+add2 r3 r2
+add2 r2 r2
+add2 r2 r2
+add2 r3 r2
+add2 r3 r1
+
+return
+
+main:
+leti r0 0xF123
+leti r1 10
+leti r2 10
+call plot
+jump -13
