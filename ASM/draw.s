c6c6b142883ef5272591aac593aa9409dc41ec08
Potatoe37 <34173967+Potatoe37@users.noreply.github.com>
2017-12-03 12:04:08 +0100

    Update draw.s

--- ASM/draw.s
+++ ASM/draw.s
@@ -5,16 +5,36 @@ jump 16 main
 
 draw:
 
+;; valeur de r7 au début du programme: à dépiler avant le return!
 push 16 r7
 
 ;; r5 est l'erreur
-sub3 r5 r1 r3
 
-let r6 r1
-dy ← y2 - y1 ;
-dx ← x2 - x1 ;
-y ← y1 ;  // rangée initiale
-e ← -dx ;  // valeur d’erreur initiale
-e(1,0) ← dy × 2;
+sub3 r6 r4 r2 ;;dy ← y2 - y1
+sub3 r5 r1 r3 ;;dx ← x2 - x1 et e ← -dx (valeur d’erreur initiale)
+;; y ← y1: on utilisera directement r2 (rangée initiale)
+;; pour ces deux la il faut voir comment gerer la pile et r7
+;; sachant que r7 sera écrasé à l'execution de call plot 
+;;donc il faudra l'empiler juste avant et le dépiler juste après (c'est pas un probleme mais c'est important^^)
+e(1,0) ← dy × 2; 
 e(0,1) ← -dx × 2;
-   
+
+loop:
+push 16 r7
+call plot
+pop 16 r7
+addi r1 1
+
+;; Ici le if à gérer je sais pas comment faire (je comprends moyen les jumps avec les adresses...)
+if (e+e(1,0) >= 0) 
+  then { add r2 1
+         
+
+cmp r3 r1
+jumpif 16 ge loop
+
+pop 16 r7
+
+return
+
+main:
