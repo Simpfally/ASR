1f4abd8fc8204ee8c0e16234540e62530c174168
Potatoe37 <34173967+Potatoe37@users.noreply.github.com>
2017-12-03 23:43:06 +0100

    Update putchar.s

--- ASM/putchar.s
+++ ASM/putchar.s
@@ -53,6 +53,44 @@ loopy:
   	jump 16 loopy
 ybreak:
 
+add3i r6 r2 4
+
+
+loopy:
+	asr3 r5 r2 5
+	shift 0 r2 7
+	add2 r5 r2
+	shift 1 r2 7
+	add2 r5 r1
+	shift 0 r5 4
+  	add2i r5 0x10000
+  	push 16 r1
+  	leti r1 0
+
+    	loopx:
+		setctr a0 r5
+    		push 32 r3
+    		andi r3 1
+  		cmp r3 0
+    		jumpif eq 16 endif1
+	  	write a0 16 r0
+    		endif1:
+		pop 32 r3 
+    		add2i r1 1
+	  	cmp r1 8
+	  	jumpif 16 gt xbreak
+	  	add2i r5 16
+		shift 0 r3 1
+	  	jump 16 loopx
+    	xbreak:
+
+  	cmp r2 r6
+  	jumpif 16 gt ybreak
+  	add2i r2 1
+  	pop 16 r1
+  	jump 16 loopy
+ybreak:
+
 pop 16 r6
 pop 16 r5
 pop 32 r4
